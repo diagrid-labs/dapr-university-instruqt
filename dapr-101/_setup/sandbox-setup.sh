acf325b642dc610b3ba1481356df812e093237fe
Marc Duiker <4191277+marcduiker@users.noreply.github.com>
2026-07-21 09:42:14 +0200

    Update to use uv for Python
    Signed-off-by: Marc Duiker <4191277+marcduiker@users.noreply.github.com>

--- dapr-101/_setup/sandbox-setup.sh
+++ dapr-101/_setup/sandbox-setup.sh
@@ -1,5 +1,7 @@
 git clone https://github.com/dapr/quickstarts.git
 docker login -u ${DockerUSER} -p ${DockerPAT}
 
-agent variable set DAPR_CLI_VERSION 1.17.0
-agent variable set DAPR_RUNTIME_VERSION 1.17.0
+agent variable set DAPR_CLI_VERSION 1.18.0
+agent variable set DAPR_RUNTIME_VERSION 1.18.0
+
+wget -qO- https://astral.sh/uv/install.sh | sh
